b0b3575428dca9afc2e7cccdc0e633fcdbe363bb
Tyler Cushing <tylercushing@wirelessprv-10-192-160-42.near.illinois.edu>
2024-05-02 23:22:23 -0500

    Added a CORS fix so we can access it from a development server

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
+CORS(app, resources={r"/search": {"origins": "http://localhost:3000"}})  # Enabling CORS for the search route
 
 DATABASE = 'master.db'
 
