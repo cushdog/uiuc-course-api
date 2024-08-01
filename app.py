a445618e738b9bbdd5d65013cf6100db6886c365
Tyler Cushing <92754165+cushdog@users.noreply.github.com>
2024-07-31 22:02:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,36 +9,7 @@ DATABASE = 'master.db'
 
 def get_db_connection():
     conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row  # This allows us to fetch rows as dictionaries
     return conn
-
-@app.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query')
-    words = query.split()
-    if len(words) == 3:
-        class_name = words[0].upper()
-        semester, year = words[1], words[2]
-        return jsonify(search_and_format_subject(class_name, semester, year))
-    else:
-        class_name, course_number = words[0].upper(), words[1]
-        semester, year = words[2], words[3]
-        return jsonify(search_and_format(class_name, course_number, semester, year))
-
-@app.route('/prereq-search', methods=['GET'])
-def prereq_search():
-    course = request.args.get('course')
-    results = search_prereqs(course)
-    return jsonify([dict(row) for row in results])
-
-def search_prereqs(course):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    query = "SELECT * FROM classes WHERE section_info LIKE ?"
-    cursor.execute(query, (f'%{course}%',))
-    results = cursor.fetchall()
-    conn.close()
-    return results
 
 def pull_from_table(class_name, course_number, semester, year):
     conn = get_db_connection()
@@ -72,6 +43,36 @@ def pull_from_table_subject(class_name, semester, year):
     if not results:
         return "Course not found"
     return results
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    words = query.split()
+    if len(words) == 3:
+        class_name = words[0].upper()
+        semester, year = words[1], words[2]
+        return jsonify(search_and_format_subject(class_name, semester, year))
+    else:
+        class_name, course_number = words[0].upper(), words[1]
+        semester, year = words[2], words[3]
+        return jsonify(search_and_format(class_name, course_number, semester, year))
+
+@app.route('/prereq-search', methods=['GET'])
+def prereq_search():
+    course = request.args.get('course')
+    results = search_prereqs(course)
+    return jsonify(results)
+
+def search_prereqs(course):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    query = "SELECT * FROM classes WHERE section_info LIKE ?"
+    cursor.execute(query, (f'%{course}%',))
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
