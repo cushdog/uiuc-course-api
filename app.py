7c85590f02d28f3d4727ac1c9c80c8d202b6b85c
cushdog <tylercush03@gmail.com>
2024-08-13 00:08:48 -0500

    Added safer seat info in

--- app.py
+++ app.py
@@ -105,8 +105,13 @@ def get_secret():
 
     secret_name = "CourseExplorerCookie"
     region_name = "us-east-1"
-
-    session = boto3.session.Session()
+    
+    session = boto3.session.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name
+    )
+    
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
